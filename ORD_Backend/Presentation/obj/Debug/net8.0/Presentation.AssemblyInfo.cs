e3379d4f41c163da64ef129426225eaae60e7b73
canhhv <havancanh2003@gmail.com>
2024-12-11 12:07:48 +0700

    Cập nhật .gitignore và loại bỏ các tệp không cần thiết

--- ORD_Backend/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ ORD_Backend/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b212038f32990be11927ffcdf556e74ca273fdce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
